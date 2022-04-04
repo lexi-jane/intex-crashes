71098ad0b313d3ff401fc758b107fbbf4000c00b
lexi-jane <alexis.transfiguracion@gmail.com>
2022-04-04 16:59:49 -0600

    first commit

---
+++ Crashes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Crashes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
