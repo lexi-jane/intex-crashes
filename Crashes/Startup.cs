b272f0b1ad38ebcfb4023c3de502512d402cb0a7
matthewmccord <matthewmccord3@gmail.com>
2022-04-04 17:15:12 -0600

    mission 07 - 02 - setup

--- Crashes/Startup.cs
+++ Crashes/Startup.cs
@@ -48,9 +48,13 @@ namespace Crashes
 
             app.UseEndpoints(endpoints =>
             {
+                
+                
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
